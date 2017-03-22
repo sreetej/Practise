c6e0daf1c2445801c0a110302aa3bcd51d95d11a
sreetej <sree.kothamasu@gmail.com>
2017-03-22 09:51:33 +0530

    added new activity recycler view

---
+++ MyPractise/app/src/main/java/com/example/sreetej/mypractise/activity/RecyclerView.java
@@ -0,0 +1,15 @@
+package com.example.sreetej.mypractise.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sreetej.mypractise.R;
+
+public class RecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+    }
+}
