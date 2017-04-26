e7f89f5e9984322c6ec45184d1448d56fe9fb5b8
sreetej <sree.kothamasu@gmail.com>
2017-04-26 16:26:13 +0530

    compoundViewSample

--- MyPractise/app/src/main/java/com/example/sreetej/mypractise/compoundVIewSample/CompoundViewActivity.java
+++ MyPractise/app/src/main/java/com/example/sreetej/mypractise/compoundVIewSample/CompoundViewActivity.java
@@ -1,5 +1,6 @@
 package com.example.sreetej.mypractise.compoundViewSample;
 
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,22 +11,25 @@ import com.example.sreetej.mypractise.R;
 
 public class CompoundViewActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compound_view);
+
+        SideSpinner fruitsSpinner;
+        fruitsSpinner = (SideSpinner)this
+                .findViewById(R.id.sidespinner_fruits);
+
+        CharSequence fruitList[] = { "Apple",
+                "Orange",
+                "Pear",
+                "Grapes" };
+        fruitsSpinner.setValues(fruitList);
+        fruitsSpinner.setSelectedIndex(1);
+
     }
 
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.activity_main, menu);
-//        return true;
-//    }
 
-    public void onClicked(View view) {
-        String text = view.getId() == R.id.view1 ? "Background" : "Foreground";
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
-    }
 }
