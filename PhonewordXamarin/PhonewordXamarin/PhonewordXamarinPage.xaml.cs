1f0b6947759b801b878c2a65c50578d34b9f5ed5
sreetej <sree.kothamasu@gmail.com>
2017-05-24 12:53:36 +0530

    Xamarin

---
+++ PhonewordXamarin/PhonewordXamarin/PhonewordXamarinPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PhonewordXamarin
+{
+	public partial class PhonewordXamarinPage : ContentPage
+	{
+		public PhonewordXamarinPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
